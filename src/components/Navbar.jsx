509712ed27e07540658b22aa5c7fd8bbc67b0985
Hansana Sandakelum <95872422+HansanaSandakelum@users.noreply.github.com>
2024-06-27 14:13:38 +0530

    constant update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useEffect,useState } from "react";
+import {styles} from '../styles'
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <div className=" w-full flex justify-between items-center max-w-7xl mx-auto">
+      <Link to='/' ></Link>
+    </div>
+  </nav>
+};
 
-export default Navbar
+export default Navbar;
